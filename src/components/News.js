c50dfe947c601ad01856d062b3b1ce4b4fd57792
Nishaojha <its.nishaojha@gmail.com>
2022-11-24 09:56:54 +0530

    Added news source

--- src/components/News.js
+++ src/components/News.js
@@ -94,6 +94,7 @@ export class News extends Component {
                   newsUrl={element.url} 
                   author = {element.author}
                   date = {element.publishedAt}
+                  source = {element.source.name}
                 />
               </div>
             );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,10 +3,11 @@ import React, { Component } from "react";
 export class NewsItem extends Component {
   
   render() {
-    let { title, description, imageUrl, newsUrl, author, date} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source} = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source}</span>
           <img src={!imageUrl?'https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132484366.jpg':imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
